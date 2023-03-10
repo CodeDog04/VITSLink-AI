00b526251d84790144dfcf135e9b6f71465c1b53
CodeDog04 <rkfox@live.com>
2023-03-10 14:59:45 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,11 +5,15 @@ pip install -r requirements.txt
 echo Cloning whisper repository...
 git clone https://github.com/openai/whisper.git
 cd whisper
-pip install -e .
+pip install -r requirements.txt
+python setup.py
+
 
 echo Cloning VITS models repository...
 git clone https://huggingface.co/spaces/sayashi/vits-models.git
-cd ..
+cd vits-models
+pip install -r requirements.txt
+
 
 echo Setup complete.
 pause
