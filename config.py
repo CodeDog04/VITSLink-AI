8d2eef132722dbda24935738cae6d16faa841b34
CodeDog04 <rkfox@live.com>
2023-03-10 13:57:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,9 @@ PTT_BUTTON = "\\" #this is the \| key above enter. Alternatively set it for anyt
 wmodel = "base" #change this model to one of those shown below
 
 
-###################################################################
-# model select ->   tiny |  base |  small |  medium |  large      #
-####################################################################################################
-#   Size   # Parameters # English-only model # Multilingual model # Required VRAM # Relative speed #
-#   tiny   #     39 M	  #  tiny.en	        #    tiny         #   ~1 GB	   #     ~32x              #
-#   base   #    74 M	  #  base.en	        #    base	        #   ~1 GB	   #     ~16x              # 
-#   small  #    244 M	  #  small.en         #    small	      #   ~2 GB	   #     ~6x               # 
-#   medium #	769 M	    #  medium.en        #    medium	      #   ~5 GB	   #     ~2x               #
-#   large  #    1550 M  #  N/A              #    large	      #   ~10 GB   #    1x                 #
-####################################################################################################
+Size	Parameters	English-only model	Multilingual model	Required VRAM	Relative speed
+tiny	  39 M	      tiny.en	              tiny	              ~1 GB	        ~32x
+base	  74 M	      base.en	              base	              ~1 GB	        ~16x
+small	  244 M	      small.en	            small	              ~2 GB	        ~6x
+medium	769 M	      medium.en	            medium	            ~5 GB	        ~2x
+large	  1550 M	    N/A	                  large	              ~10 GB	       1x
