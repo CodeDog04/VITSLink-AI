8a8ad28d3a8bd61510d18ccaad84721b885f3236
CodeDog04 <rkfox@live.com>
2023-03-10 15:27:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,51 @@
-api_key = "INSERT API KEY HERE"
-PTT_BUTTON = "\\" #this is the \| key above enter. Alternatively set it for anything
-wmodel = "base" #change this model to one of those shown below
+####- CONFIG -####
+api_key = "INPUT API KEY HERE" #Add API KEY
+PTT_BUTTON = "\\" #Change to the key you want to hit
+wmodel = "base" #Change whisper model
+tts_model = "tts-Kyaru" #change vits-tts model
+#language = "insert language here" (currently hardcoded to japanese only until I do more testing)
+
+####- tts_models -####
+# tts-Yuni
+# tts-Kyoka
+# tts-Ameth
+# tts-Hatsune
+# tts-Eriko
+# tts-Pecorine
+# tts-Kokoro
+# tts-Kyaru
+# tts-Nishikigi Chisato
+# tts-Takina Inoue
+# tts-ayaka-jp
+# tts-nahida-jp
+# tts-Natsume Iroha
+# tts-Misono Mika
+# tts-Kasumizawa Miyu
+# tts-Kakudate Karin
+# tts-Ichinose Asuna
+# tts-Shirasu Azusa
+# tts-Tendou Alice
+# tts-Sunaookami Shiroko
+# tts-Takanasi Hosino
+# tts-Sorasaki Hina
+# tts-Shiromi Iori
+# tts-Kuda Izuna
+# tts-Hayase Yuuka
+# tts-Doomfist
+# tts-Echo
+# tts-Zenyatta
+
+####- Languages -####
 
 
-Size    Parameters  English-only model   Multilingual model  Required VRAM   Relative speed
-tiny    39 M        tiny.en             tiny               ~1 GB           ~32x
-base    74 M        base.en             base               ~1 GB           ~16x
-small   244 M       small.en            small              ~2 GB           ~6x
-medium  769 M       medium.en           medium             ~5 GB           ~2x
-large   1550 M      N/A                 large              ~10 GB          1x
+####- Whisper TTS models -####
+###################################################################
+# model select ->   tiny |  base |  small |  medium |  large      #
+####################################################################################################
+#   Size   # Parameters # English-only model # Multilingual model # Required VRAM # Relative speed #
+#   tiny   #     39 M	#    tiny.en	        #    tiny         #   ~1 GB	   #     ~32x          #
+#   base   #    74 M	#    base.en	        #    base	      #   ~1 GB	   #     ~16x          # 
+#   small  #    244 M	#    small.en           #    small	      #   ~2 GB	   #     ~6x           # 
+#   medium #	769 M	#    medium.en          #    medium	      #   ~5 GB	   #     ~2x           # 
+#   large  #    1550 M	#    N/A                #    large	      #   ~10 GB   #      1x           # 
+####################################################################################################
