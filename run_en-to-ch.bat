014cc44e82c83e3f1639542573b2da0d75405a2f
CodeDog04 <rkfox@live.com>
2023-03-10 20:19:18 -0500

    Delete run_en-to-ch.bat

--- run_en-to-ch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting English to Japanese Text to Speech Translator
-echo Starting VITS...
-cd vits-models
-start cmd /k python app.py --api
-
-cd ..
-start cmd /k python english_to_chinese.py
